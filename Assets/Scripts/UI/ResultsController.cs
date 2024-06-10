89d3902126fe531e2e5017d458897b08e761a9fe
Ryan Prothero <ryan.prothero@outlook.com>
2024-06-11 06:20:30 +0900

    Add simple results screen
    Not pretty, but gets the job done for now

---
+++ Assets/Scripts/UI/ResultsController.cs
@@ -0,0 +1,37 @@
+﻿using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace RhythmGame
+{
+    public class ResultsController : MonoBehaviour
+    {
+        [SerializeField]
+        public Canvas resultsCanvas;
+
+        [SerializeField]
+        private TextMeshProUGUI greatCounter;
+        [SerializeField]
+        private TextMeshProUGUI okayCounter;
+        [SerializeField]
+        private TextMeshProUGUI missCounter;
+
+        public void SetResults(int greats, int okays, int misses)
+        {
+            greatCounter.text = greats.ToString();
+            okayCounter.text = okays.ToString();
+            missCounter.text = misses.ToString();
+        }
+
+        public UniTask Display()
+        {
+            resultsCanvas.gameObject.SetActive(true);
+            return UniTask.WaitWhile(() => resultsCanvas.gameObject.activeSelf);
+        }
+
+        public void Hide()
+        {
+            resultsCanvas.gameObject.SetActive(false);
+        }
+    }
+}
